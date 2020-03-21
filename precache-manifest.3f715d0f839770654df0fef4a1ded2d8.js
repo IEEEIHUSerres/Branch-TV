7a92483d09974cba03833f2e18be5164f7be8bee
Iordanis Kostelidis <Kostelidis@IEEE.org>
2020-03-21 15:03:47 +0200

    Updates

---
+++ precache-manifest.3f715d0f839770654df0fef4a1ded2d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e61629d5b9986daa0730a1825b5dc6",
+    "url": "/Branch-TV/index.html"
+  },
+  {
+    "revision": "121a6024c2427948c200",
+    "url": "/Branch-TV/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "832e9ed418131d56e4c9",
+    "url": "/Branch-TV/static/js/2.5d8d02de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Branch-TV/static/js/2.5d8d02de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121a6024c2427948c200",
+    "url": "/Branch-TV/static/js/main.229c3bbe.chunk.js"
+  },
+  {
+    "revision": "aa6f3412057a322932a5",
+    "url": "/Branch-TV/static/js/runtime-main.3a6b8db3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Branch-TV/static/media/logo.5d5d9eef.svg"
+  }
+]);
